1a06b816dc5e3e79f7566254e208171a9e847226
Nail Kalimov <nailkalimov@gmail.com>
2025-06-27 17:31:27 +0400

    Refactoring models

--- usd/models.py
+++ usd/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 
-class USD(models.Model):
+class Currency(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.code
+
+
+class CurrencyRate(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     time = models.DateTimeField()
     value = models.FloatField()
 
     class Meta:
         ordering = ('-time',)
+        # unique_together = ('currency', 'time')
 
     def __str__(self):
-        return str(self.value)
+        return f'{self.currency}: {self.value} @ {self.time}'
--- usd/views.py
+++ usd/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django_ratelimit.decorators import ratelimit
 from django.shortcuts import render
-from .models import USD
+from .models import Currency, CurrencyRate
 from .utils import get_actual_rate_usd
 
 
@@ -15,14 +15,18 @@ def get_current_usd(request):
         return JsonResponse({'Error': 'Request limit exceeded, please try again in 10 seconds.'}, status=429)
     else:
         data = get_actual_rate_usd()
-        USD.objects.create(time=data['Date'], value=data['Valute']['USD']['Value'])
-        last_ten_requests = USD.objects.all()[:10]
+        usd_currency, _ = Currency.objects.get_or_create(code='USD', defaults={'name': 'US Dollar'})
+        CurrencyRate.objects.create(
+            currency=usd_currency,
+            time=data['Date'],
+            value=data['Valute']['USD']['Value']
+        )
+        last_ten_requests = CurrencyRate.objects.filter(currency=usd_currency).order_by('-time')[:10]
         res = {
             'Value': data['Valute']['USD']['Value'],
             'CurrentTime': data['Date'],
-            'Last_10_Value':
-                [
-                    {'Value': i.value, 'Time': i.time} for i in last_ten_requests
-                ]
+            'Last_10_Value': [
+                {'Value': i.value, 'Time': i.time} for i in last_ten_requests
+            ]
         }
         return JsonResponse(res, status=200)
